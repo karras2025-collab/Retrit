6f58154dfeb5f3943634987f2e64de66f4705b05
karras2025-collab <ivan@gmail.com>
2026-01-27 23:58:25 +0300

    ui: move Hero CTA buttons 10% lower

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -46,7 +46,7 @@ export const Hero: React.FC = () => {
         </button>
 
         {/* CTA Buttons */}
-        <div className="absolute inset-0 z-10 flex items-end justify-center pb-[35%] md:pb-[30%]">
+        <div className="absolute inset-0 z-10 flex items-end justify-center pb-[25%] md:pb-[20%]">
           <div className="flex flex-col sm:flex-row gap-4">
             <a
               href="#program"
